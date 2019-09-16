6cdf8c2fe52e991bd01ab759c86e91cde518937f
sunnykim91 <tjsgnl2002@gmail.com>
2019-09-16 17:00:47 +0900

    stoneweiht

---
+++ 알고리즘/leetcode/stoneweight.js
@@ -0,0 +1,29 @@
+let i = 0;
+var lastStoneWeight = function (stones) {
+  
+  while(true) {
+    stones.sort((a, b) => b - a);
+    if(stones.length <= 1){
+      break;
+    } else {
+      stoneChange(stones);
+    }
+    i++;
+  }
+  return stones;
+};
+
+function stoneChange(arr){
+  if (arr[i] !== arr[i + 1]) {
+      arr[i] = arr[i] - arr[i + 1];
+      arr.splice(i + 1, 1);
+    } else {
+      arr.splice(i, 2);
+    }
+    console.log(arr);
+  
+  return arr;
+}
+
+
+console.log(lastStoneWeight([2, 7, 4, 1, 8, 1]));  // 8 7 4 2 1 1
