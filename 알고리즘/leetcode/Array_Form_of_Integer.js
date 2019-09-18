58f8eb364b73ff47ec6922bcdb8ebebfea504924
sunnykim91 <tjsgnl2002@gmail.com>
2019-09-18 16:31:56 +0900

    array form of integer update

---
+++ 알고리즘/leetcode/Array_Form_of_Integer.js
@@ -0,0 +1,34 @@
+var addToArrayForm = function (A, K) {
+  let arr = '';
+  let arr2 = '';
+  let tempArr = [];
+  let resultNum = '';
+  for (let i = 0; i < A.length; i++){
+    arr += A[i];
+  }
+  if( Number.isSafeInteger(Number(arr))){
+    resultNum = +arr + K;
+  } else {
+    K = K + '';
+    fTempArr = A.splice(0, A.length - K.length - 1);
+    rTempArr = A.splice(A.length - K.length - 1, A.length);
+    console.log(rTempArr);
+    console.log(fTempArr);
+    for (let o = 0; o < rTempArr.length; o++) {
+      arr2 += rTempArr[o];
+    }
+    resultNum = parseInt(arr2) + parseInt(K);
+  }
+  console.log(resultNum);
+  let resultArr = resultNum + '';
+  let finalResult = [];
+  for(let j = 0; j < resultArr.length; j++ ){
+    finalResult.push(+resultArr[j]);
+  }
+
+  return finalResult;
+};
+
+// console.log(addToArrayForm([1, 2, 0, 0], 34));
+// console.log(addToArrayForm([2, 7, 4], 181));
+console.log(addToArrayForm([1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3], 516));
