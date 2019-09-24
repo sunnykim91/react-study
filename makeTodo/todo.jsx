970345e31d4e4847c5024ad21a13934b55a82711
sunnykim91 <tjsgnl2002@gmail.com>
2019-09-24 15:05:25 +0900

    react todo init

---
+++ makeTodo/todo.jsx
@@ -0,0 +1,14 @@
+const React = require('react');  // 쪼갠파일에서 필요한 패키지나 라이브러리 가져오는 부분
+const { Component } = React;
+
+class Todo extends Component {
+  state = {
+    text: 'Hello, webpack',
+  };
+
+  render() {
+    return <div>{this.state.text}</div>
+  }
+}
+
+module.exports = Todo; // 쪼갠 파일을 밖에서도 사용하게 해줌.
